4757ef8b59b03be1d923e563d2d8aa9f0cc4063d
Tonyy203 <nguyentuantai630@gmail.com>
2024-11-15 22:57:15 -0800

    lesson06

---
+++ Session_06/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+namespace Session_06.Models
+{
+    public class Book
+    {
+        [DisplayName("Mã sách")]
+        [StringLength(10)]
+        public string BookId { get; set; }
+        [DisplayName("Tên sách")]
+        [StringLength(200)]
+        public string Title { get; set; }
+        [DisplayName("Tên sách")]
+        [StringLength(200)]
+        public string Author { get; set; }
+        [DisplayName("Tác giả")]
+        [StringLength(100)]
+        public int? Release { get; set; }
+        [DisplayName("Giá")]
+        public double? Price { get; set; }
+        [DisplayName("Mô tả")]
+        public string Description {  get; set; }
+        [DisplayName("Hình ảnh")]
+        public string Picture {  get; set; }
+        [DisplayName("Mã nhà sản xuất")]
+        public int? Publishe { get; set; }
+        [DisplayName("Mã loại")]
+        public int? CategoryId { get; set; }
+        //Tạo các quan hệ giữa các thực thể
+        public Categogy Categogy { get; set; }
+        public Publisher Publisher { get; set; }
+    }
+}
