98ec250c277ccf7b9ce8e1893014b384cb7a2703
Tonyy203 <132805217+Tonyy203@users.noreply.github.com>
2024-12-21 06:16:34 +0700

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevXuongMoc.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string? Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? FullName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string? Phone { get; set; }
+
+        [StringLength(100)]
+        public string? Address { get; set; }
+
+        [StringLength(100)]
+        public string? Avatar { get; set; }
+
+        public string? Birthday { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string? Gender { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string? Password { get; set; }
+
+        [StringLength(100)]
+        public string? Facebook { get; set; }
+    }
+}
